893b6cf18f1080d35cfd43432f4446f1f705de98
Andre <andregozubuyuk@gmail.com>
2023-04-24 22:34:23 +0300

    Updated files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const connectDB = require("./config/db");
@@ -15,11 +16,19 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// cors middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5000", "http://localhost:3000"],
+    credentials: true,
+  })
+);
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the RandomIdeas API" });
 });
 
-const ideasRouter = require("./routes/ideas.js");
+const ideasRouter = require("./routes/ideas");
 app.use("/api/ideas", ideasRouter);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
